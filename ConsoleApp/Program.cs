f0d13f1bb2d3f614347bc1c076426465a47909cd
Alexandra Johansson <alexandra_johansson111@live.se>
2020-09-22 15:34:14 +0200

    UPG3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Models;
+using SharedLibraries.Services;
+
 
 namespace ConsoleApp
 {
     class Program
     {
+        //connectionstring till iot-apparaten i Azure
+        private static readonly string _conn = "HostName=EC-WEB20-AJ.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=FQFt0u9cyO0SEDTU7+zDQHAogBVuOPeag2O49QiDaPw=";
+
+        //Skapar en iot Device
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //get.Awaiter är istället för att ändra om Main-funtionen till async
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+
+            DeviceService.RecieveMessageAsyn(deviceClient).GetAwaiter();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SharedLibraries/Services/DeviceService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibraries.Models;
+
+namespace SharedLibraries.Services
+{
+    //hela klassen är static, vilket innebär att allt i klassen också måste vara static
+    public static class DeviceService
+    {
+        //Kan inte byta ut Random-typen. Fastlåst. ett Random-objekt
+        private static readonly Random rnd = new Random();
+
+        
+        //funtion som sänger ett asyncront meddelande 
+        //DeviceClient kommer från Microsoft.Azure.Devices.Client;
+
+        //är en device client = IoT device
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+
+            //evighetsloop
+            while (true)
+            {
+                //Skapar ett nytt Temperatur-objekt
+                var data = new TemperatureModel
+                {
+                    Temperature = rnd.Next(20, 30),
+                    Humidity = rnd.Next(30, 40)
+                };
+
+                //formatera om objektet ovan till ett Json-objekt
+                //alla kan läsa json, därför är det bra att konvertera till json-format
+                var json = JsonConvert.SerializeObject(data);
+
+                //det vi vill skicka kallas för payload
+                //Message kommer från Microsoft.Azure.Devices.Client;
+                //formatera meddlandet till UTF8. Omvandla till bytes så att datorn förstår. SKickas på nätet genom 1or och 0or
+                var payload = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(json));
+
+                //ALLTID när det står async med måste det vara en await!!
+                await deviceClient.SendEventAsync(payload);
+
+                Console.WriteLine($"Message sent: {json}");
+
+                //vänta med svaret i x-antal sekunder
+                await Task.Delay(5 * 1000);
+            }
+
+        }
+
+        //är en device client = IoT device
+        public static async Task RecieveMessageAsyn(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+                var payload = await deviceClient.ReceiveAsync();
+
+                if(payload == null)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Message recieved {Encoding.UTF8.GetString(payload.GetBytes())}");
+
+                //tar bort meddelandet från hubben
+                await deviceClient.CompleteAsync(payload);
+
+            }
+        }
+
+        //är en Service(utför någonting, tex mobiltelefon) client = simulerar Ioy Hub
+        public static async Task SendMessageToDeviceAsync(ServiceClient serviceClient, string targetDevice, string message)
+        {
+            var payload = new Microsoft.Azure.Devices.Message(Encoding.UTF8.GetBytes(message));
+
+            await serviceClient.SendAsync(targetDevice, payload);
+        }
+    }
+}
